2a0ba2c78c2921e475a2d7def254644951dde48c
优的灰烬 <c.m.i@qq.com>
2015-02-25 16:43:13 +0800

    rename folder

---
+++ Assets/Script/SoldierMoving.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+//cleanLevel = 0;
+public class SoldierMoving : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (!hit.collider.name.Equals("Terrain"))
+                {
+                    return;
+                }
+                Vector3 point = hit.point;
+                transform.LookAt(new Vector3(point.x, transform.position.y, point.z));
+                agent.SetDestination(point);
+            }
+        }
+
+
+        if (agent.remainingDistance == 0)
+        {
+            animation.Play("Idle");
+        }
+        else
+        {
+            animation.Play("Run");
+        }
+
+    }
+}
